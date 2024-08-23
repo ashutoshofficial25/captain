1ab96f517af5175cea7489d8203be2e88002a472
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-08-23 18:23:41 +0530

    otp

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,11 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+
+import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
 import Mobile from './screens/mobile';
 
 function App(): React.JSX.Element {
