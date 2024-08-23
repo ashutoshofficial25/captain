3487220172c2a29c94932558f3e1791a46694935
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-08-23 12:45:33 +0530

    initial pages

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Welcome to my app</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/screens/mobile.tsx
@@ -0,0 +1,9 @@
+import {StyleSheet, View} from 'react-native';
+
+export default function Mobile() {
+  return <View>Enter your phone number</View>;
+}
+
+const styles = StyleSheet.create({
+  h2: {},
+});
